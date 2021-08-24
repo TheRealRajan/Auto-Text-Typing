6ce506145199e47af2e6f8e24fddab51438c6b7d
therealrajan <therealrajan@gmail.com>
2021-08-24 13:30:07 +0545

    Added local files to remote repo

---
+++ script.js
@@ -0,0 +1,36 @@
+const textEl = document.querySelector('.text')
+const speeedEl = document.querySelector('.speed')
+const changeText = document.querySelector('.changeText')
+let text = "This sentence is magically typing itself !"
+
+let idx = 1
+let speed = 300 / speeedEl.value
+
+writeText()
+
+function writeText(){
+    textEl.innerText = text.slice( 0, idx )
+    idx++
+
+    if(idx > text.length){
+        idx = 1
+    }
+    setTimeout(() => writeText(), speed);
+}
+
+speeedEl.addEventListener('input', (e)=>{
+    
+    if(e.target.value>5){
+        e.target.value = 5
+    }
+    speed =  300 / e.target.value
+    console.log(speed)
+})
+
+changeText.addEventListener('input', (e)=> {
+    text = e.target.value
+    
+    if(text.length === 0){
+        text = "Type something in there my dude"
+    }
+})
